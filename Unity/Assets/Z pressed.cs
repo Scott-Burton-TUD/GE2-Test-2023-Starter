ea45baf50a17b9d2aea1338f2564573ae136e469
Scott-Burton-TUD <c20341223@mytudublin.ie>
2023-04-25 11:48:40 +0100

    Z pressed
    Z to release creature

---
+++ Unity/Assets/Z pressed.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zpressed : MonoBehaviour
+{
+    public Transform creature;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        creature.transform.position = new Vector3 (0, 0, 0);
+    }
+}
